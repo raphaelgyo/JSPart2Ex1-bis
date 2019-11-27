1948b79813a493630c4025a80955f8688339336c
raphaelgyo <raphael.granizo@gmail.com>
2019-11-27 19:37:53 +0100

    1st attempt

---
+++ assets/js/javascript.js
@@ -0,0 +1,8 @@
+//Affecte la nouvelle image lorsque la souris survole l'élément
+function overPicture(element) {
+element.setAttribute('src', 'assets/img/babyYoda.jpg');
+}
+//Affecte l'image de départ lorsque la souris ne survole plus l'élément
+function outPicture(element) {
+element.setAttribute('src', 'assets/img/gizmo.jpg');
+}
